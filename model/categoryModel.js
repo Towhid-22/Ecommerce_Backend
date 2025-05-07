6546bd6d02a8667f4e654fe6d4356e9fc61656c2
Towhidul Islam <towhid212037@gmail.com>
2025-05-07 16:40:10 +0600

    update category done

--- model/categoryModel.js
+++ model/categoryModel.js
@@ -4,9 +4,10 @@ const categoryModel = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "Category name is required"],
-    maxlength: [20, "Username must be less than 20 characters"],
+    maxlength: [50, "Username must be less than 50 characters"],
     minlength: [3, "Username must be more than 3 characters"],
     trim: true,
+    unique: true,
   },
   image: {
     type: String,
@@ -15,6 +16,11 @@ const categoryModel = new mongoose.Schema({
   description: {
     type: String,
     trim: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
   },
 });
 
--- router/api/category.js
+++ router/api/category.js
@@ -3,50 +3,18 @@ const path = require("path");
 const {
   addcategoryController,
   getAllCategories,
+  singleCategory,
+  deletecategory,
+  updatecategory,
 } = require("../../controllers/categoryController");
 const router = express.Router();
-const multer = require("multer");
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./uploads");
-  },
-  filename: function (req, file, cb) {
-    const extensionName = file.mimetype.split("/");
-    console.log(extensionName);
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(
-      null,
-      file.fieldname +
-        "-" +
-        uniqueSuffix +
-        "." +
-        extensionName[extensionName.length - 1]
-    );
-  },
-});
-// check file type for upload
-function checkFileType(file, cb) {
-  const filetypes = /jpeg|jpg|png|gif|mp4/;
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = filetypes.test(file.mimetype);
-
-  if (mimetype && extname) {
-    return cb(null, true);
-  } else {
-    cb("Error: Images only! (jpeg, jpg, png, gif)");
-  }
-}
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 1000000 },
-  fileFilter: function (req, file, cb) {
-    checkFileType(file, cb);
-  },
-});
+const upload = require("../../helpers/uploadimage");
 
 // localhost:4000/api/v1/category/addcategory
 router.post("/addcategory", upload.single("avatar"), addcategoryController);
 router.get("/getcategories", getAllCategories);
+router.get("/singleCategory/:slug", singleCategory);
+router.delete("/deletecategory/:id", deletecategory);
+router.patch("/updatecategory/:id", upload.single("avatar"), updatecategory);
 
 module.exports = router;
